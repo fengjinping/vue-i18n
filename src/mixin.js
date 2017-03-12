e78b77058ba8620da72aaf04c00d73e7874f23f9
iceStone <ice@micua.com>
2017-03-12 14:22:29 +0800

    :bug: bug(mixin): fix cannot create VueI18n instance error when use vue-router-sync

--- src/mixin.js
+++ src/mixin.js
@@ -6,36 +6,39 @@ import { isPlainObject, warn } from './util'
 export default {
   computed: {
     $t () {
-      if (!this.$i18n) {
+      const i18n = this.$i18n || this
+      if (!i18n) {
         throw Error(`Failed in $t due to not find VueI18n instance`)
       }
       // add dependency tracking !!
-      const locale: string = this.$i18n.locale
-      const messages: Messages = this.$i18n.messages
+      const locale: string = i18n.locale
+      const messages: Messages = i18n.messages
       return (key: string, ...args: any): string => {
         return this.$i18n._t(key, locale, messages, this, ...args)
       }
     },
 
     $tc () {
-      if (!this.$i18n) {
+      const i18n = this.$i18n || this
+      if (!i18n) {
         throw Error(`Failed in $tc due to not find VueI18n instance`)
       }
       // add dependency tracking !!
-      const locale: string = this.$i18n.locale
-      const messages: Messages = this.$i18n.messages
+      const locale: string = i18n.locale
+      const messages: Messages = i18n.messages
       return (key: string, choice?: number, ...args: any): string => {
         return this.$i18n._tc(key, locale, messages, this, choice, ...args)
       }
     },
 
     $te () {
-      if (!this.$i18n) {
+      const i18n = this.$i18n || this
+      if (!i18n) {
         throw Error(`Failed in $te due to not find VueI18n instance`)
       }
       // add dependency tracking !!
-      const locale: string = this.$i18n.locale
-      const messages: Messages = this.$i18n.messages
+      const locale: string = i18n.locale
+      const messages: Messages = i18n.messages
       return (key: string, ...args: any): boolean => {
         return this.$i18n._te(key, locale, messages, ...args)
       }
